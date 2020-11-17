0290c9321ff0c9ae81236d4b6adfd23596856885
Yuri Isbener <yurisuhett@gmail.com>
2020-11-17 16:32:08 -0300

    Atividade Feita

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Contador_de_0_a_50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Você consegue advinhar a idade desse famoso?");
+
+            int idade;
+
+            do
+            {
+                Console.WriteLine("Qual a idade dele?");
+                idade = int.Parse(Console.ReadLine());
+                
+            }while(idade != 47);
+
+            Console.WriteLine("Parabéns você acertou");
+        }
+    }
+}
